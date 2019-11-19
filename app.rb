60ddc93c2f06a68511e0bfbd1b80cd289a1a8f7a
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-19 22:37:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,11 @@ class App < Sinatra::Base
   get '/name' do
   "My name is Candice"
  end
+ 
+  get '/hometown'
+  "My hometown is Chicago"
+  end
+  
+  get '/favorite-song'
+  "My favorite song is unrung"
 end
